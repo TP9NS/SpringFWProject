1ec4b808c42a7363f86aaf2c013e0acb424d3733
TP9NS <pshccxx@gmail.com>
2025-04-24 15:48:53 +0900

    spring first commit

---
+++ src/main/java/myspring/di/xml/Hello.java
@@ -0,0 +1,47 @@
+package myspring.di.xml;
+
+import java.util.List;
+
+public class Hello {
+	String name;
+	Printer printer;
+	List<String> names;
+
+	public Hello() {
+		System.out.println(this.getClass().getName());
+	}
+
+	public Hello(String name, Printer printer) {
+		this.name = name;
+		this.printer = printer;
+	}
+
+	public List<String> getNames() {
+		return this.names;
+	}
+
+	public void setNames(List<String> list) {
+		this.names = list;
+		System.out.println("setnames 호출 됐어~!");
+	}
+
+	public void setName(String name) {
+		this.name = name;
+		System.out.println("setname 호출 됐어!~ "+name);
+	}
+
+	public void setPrinter(Printer printer) {
+		this.printer = printer;
+		System.out.println("setprinter 호출 됐어~!"+printer.getClass().getName());
+	}
+
+	public String sayHello() {
+		return "Hello " + name;
+		
+	}
+
+	public void print() {
+		this.printer.print(sayHello());
+	}
+
+}

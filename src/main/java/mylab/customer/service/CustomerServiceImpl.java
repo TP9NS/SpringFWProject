b3f5e0fd97c5e4bd6cf55bc1940810c94531f254
TP9NS <pshccxx@gmail.com>
2025-05-04 17:07:27 +0900

    1-4

---
+++ src/main/java/mylab/customer/service/CustomerServiceImpl.java
@@ -0,0 +1,27 @@
+package mylab.customer.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mylab.customer.dao.CustomerDAO;
+import mylab.customer.vo.CustomerVO;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private CustomerDAO customerDAO;
+
+    @Override
+    public CustomerVO getCustomerInfo(int id) {
+        return customerDAO.selectCustomer(id);
+    }
+
+    @Override
+    public List<CustomerVO> getAllCustomerList() {
+        return customerDAO.selectAllCustomer();
+    }
+
+}
